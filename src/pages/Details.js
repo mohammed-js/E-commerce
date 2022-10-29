01f63e4d338e268081d917033ed1599dac26a14b
Mohammed Moustafa <mohammedmoustafa1100@gmail.com>
2022-10-29 16:28:57 +0200

    second commit

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
 import { useParams } from "react-router-dom";
+import Navbar from "../sections/Navbar";
 function Details() {
   const [item, setItem] = useState();
   useEffect(() => {
@@ -14,53 +15,60 @@ function Details() {
   const { id } = params;
 
   return (
-    <div className="details-top-container">
-      {item ? (
-        <div className="details-mid-container">
-          <div className="details-low-container">
-            <img
-              alt="product"
-              src={item?.images[0]}
-              className="main-img-section"
-            ></img>
+    <>
+      <Navbar></Navbar>
+      <div className="details-top-container">
+        {item ? (
+          <div className="details-mid-container">
+            <div className="details-low-container">
+              <img
+                alt="product"
+                src={item?.images[0]}
+                className="main-img-section"
+              ></img>
+            </div>
+            <div className="details-section">
+              <div>
+                <span style={{ fontWeight: "bold", fontSize: "40px" }}>
+                  {item?.title}
+                </span>
+              </div>
+              <div className="topic">
+                Brand: <span className="description">{item?.brand}</span>
+              </div>
+              <div className="topic">
+                Category: <span className="description">{item?.category}</span>
+              </div>
+              <div className="topic">
+                Price: <span className="description">{`${item?.price}$`}</span>
+              </div>
+              <div className="topic">
+                Rating: <span className="description">{item?.rating}</span>
+              </div>
+              <div className="topic">
+                Description:{" "}
+                <span className="description">{item?.description}</span>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  paddingTop: "20px",
+                }}
+              >
+                {item?.images.map((image) => (
+                  <img src={image} alt="img" className="small-img"></img>
+                ))}
+              </div>
+            </div>
           </div>
-          <div className="details-section">
-            <div>
-              <span style={{ fontWeight: "bold", fontSize: "40px" }}>
-                {item?.title}
-              </span>
-            </div>
-            <div className="topic">
-              Brand: <span className="description">{item?.brand}</span>
-            </div>
-            <div className="topic">
-              Category: <span className="description">{item?.category}</span>
-            </div>
-            <div className="topic">
-              Price: <span className="description">{`${item?.price}$`}</span>
-            </div>
-            <div className="topic">
-              Rating: <span className="description">{item?.rating}</span>
-            </div>
-            <div className="topic">
-              Description:{" "}
-              <span className="description">{item?.description}</span>
-            </div>
-            <div
-              style={{ display: "flex", flexWrap: "wrap", paddingTop: "20px" }}
-            >
-              {item?.images.map((image) => (
-                <img src={image} alt="img" className="small-img"></img>
-              ))}
-            </div>
-          </div>
-        </div>
-      ) : (
-        <Box sx={{ display: "flex" }}>
-          <CircularProgress size={200} />
-        </Box>
-      )}
-    </div>
+        ) : (
+          <Box sx={{ display: "flex" }}>
+            <CircularProgress size={200} />
+          </Box>
+        )}
+      </div>
+    </>
   );
 }
 
